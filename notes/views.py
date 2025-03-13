fe9655b78169dcaa95d04e5e8f641fd72e2115fb
Amina-Delali <aimosta.official@gmail.com>
2025-03-13 16:24:30 +0100

    without template - we will add template later

--- notes/views.py
+++ notes/views.py
@@ -4,16 +4,16 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
-# def index(request):
-#     now = datetime.now()
-#     html = f'''
-#     <html>
-#         <body>
-#             <h1>Hello from Vercel!</h1>
-#             <p>The current time is { now }.</p>
-#         </body>
-#     </html>
-#     '''
-#     return HttpResponse(html)
 def index(request):
-    return render(request, 'notes/index.html')
+    now = datetime.now()
+    html = f'''
+    <html>
+        <body>
+            <h1>Hello from Try Django 🧑‍💻</h1>
+            <p>The current time is { now }.</p>
+        </body>
+    </html>
+    '''
+    return HttpResponse(html)
+# def index(request):
+#     return render(request, 'notes/index.html')
