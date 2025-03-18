a054d80a4aa0638fbd8110c9d11fed3c59beaf3a
Amina-Delali <aimosta.official@gmail.com>
2025-03-18 07:09:17 +0100

    some config

--- notes/urls.py
+++ notes/urls.py
@@ -22,16 +22,17 @@ from notes.views import *
 # router.register(r'users', UserViewSet)
 
 
-from rest_framework import routers
+# from rest_framework import routers
 
-from tutorial.quickstart import views
+# from tutorial.quickstart import views
 
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
+# router = routers.DefaultRouter()
+# router.register(r'users', views.UserViewSet)
+# router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
     path('', index),
     # path('register/',RegisterView.as_view(),'register'),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    
 ]
