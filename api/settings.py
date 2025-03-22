0be193b8295b9aecbff5099d3ca58de5d7d477c4
Amina-Delali <aimosta.official@gmail.com>
2025-03-22 07:15:49 +0100

    static_roos = staticfiles simply

--- api/settings.py
+++ api/settings.py
@@ -147,7 +147,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles','static'))
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
+
+STATIC_ROOT = 'staticfiles'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 pip3 install -r requirements.txt
 python3.12 manage.py collectstatic --noinput
-python3.12 manage.py makemigration --noinput
+python3.12 manage.py makemigrations --noinput
 python3.12 manage.py migrate --noinput
