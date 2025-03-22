4974a838a0975f38a43ad948ee52ed1a1ce4ef2d
Amina-Delali <aimosta.official@gmail.com>
2025-03-22 07:53:53 +0100

    managing statifiles

--- api/settings.py
+++ api/settings.py
@@ -149,10 +149,12 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 # STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles','static'))
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
 
-STATIC_ROOT = 'staticfiles'
-STATIC_URL = 'static/'
+# Static asset configuration
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles','static'))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-
