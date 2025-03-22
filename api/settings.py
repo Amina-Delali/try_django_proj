855d2717200ce08103566c008f3f1ec18c11c6bc
Amina-Delali <aimosta.official@gmail.com>
2025-03-22 13:28:43 +0100

    second project settings - static in root

--- api/settings.py
+++ api/settings.py
@@ -163,5 +163,5 @@ STATICFILES_DIRS = (
 )
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR , 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build','static')
+STATICFILES_DIRS = [BASE_DIR/'static',]
+STATIC_ROOT = BASE_DIR/'staticfiles'
