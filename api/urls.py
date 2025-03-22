c1ec9656252f977e43e9c1df658b662b123927c9
Amina-Delali <aimosta.official@gmail.com>
2025-03-22 07:56:45 +0100

    add stuff in urls

--- api/urls.py
+++ api/urls.py
@@ -27,6 +27,9 @@ from rest_framework import routers
 from notes import views
 from django.contrib import admin
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
@@ -38,3 +41,6 @@ urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)    
