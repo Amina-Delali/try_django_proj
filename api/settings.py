4b277921865db8c2527d61a2dcb1f3da3d2354ab
Amina-Delali <aimosta.official@gmail.com>
2025-03-22 12:54:12 +0100

    first project trial- webappconfig

--- api/settings.py
+++ api/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'notes',
-    'notes.apps.NotesConfig',
+    'notes.apps.WebappConfig',
     'rest_framework',
 
 ]
