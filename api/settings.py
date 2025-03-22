6c536a1d8a8c6396252639348235e5906390ade8
Amina-Delali <aimosta.official@gmail.com>
2025-03-22 15:36:14 +0100

    fourth project settings - static in ui folder

--- api/settings.py
+++ api/settings.py
@@ -163,6 +163,6 @@ STATICFILES_DIRS = (
 )
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "ui/static")]
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "ui/staticfiles")
