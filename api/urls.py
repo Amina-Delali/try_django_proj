69f5e6efaff1980d2b53e64662beef033469870f
Amina-Delali <aimosta.official@gmail.com>
2025-03-23 12:03:20 +0100

    changed table + settings in url

--- api/urls.py
+++ api/urls.py
@@ -54,8 +54,15 @@ from django.contrib import admin
 from django.urls import path, include
 from notes import urls as notes_urls
 
+
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('notes/', include(notes_urls)),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
